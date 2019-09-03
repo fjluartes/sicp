a5a32fc91ff9532b45be577c864e5a8e2dfb88dd
Fred Luartes <fjluartes@gmail.com>
2019-09-03 17:09:15 +0800

    Initial commit

---
+++ ch1/ex1.1.rkt
@@ -0,0 +1,4 @@
+#lang sicp
+(define (square a)
+  (* a a))
+(square 12)
---
+++ ch1/ex1.3.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+(define (square a)
+  (* a a))
+(define (sum-squares a b)
+  (+ (square a) (square b)))
+(define (sq2largest a b c)
+  (cond ((and (>= a b) (>= b c)) (sum-squares a b))
+        ((and (>= a b) (>= c b)) (sum-squares a c))
+        ((and (>= b c) (>= c a)) (sum-squares b c))))
+(sq2largest 5 3 3)
+(sq2largest 9 7 8)
+(sq2largest 5 4 6)
