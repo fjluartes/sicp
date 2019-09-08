f536b12becb2d67b35f283c23c6bd16616a77952
fjgl <fjluartes@gmail.com>
2019-09-08 21:21:01 +0800

    Adds ex1.2

--- ch1/ex1.1.rkt
+++ ch1/ex1.1.rkt
@@ -1,4 +1,13 @@
 #lang sicp
+;; Ex. 1.1 
 (define (square a)
   (* a a))
-(square 12)
+
+(define (sum-of-squares a b)
+  (+ (square a) (square b)))
+
+(define (f a)
+  (sum-of-squares (+ a 1) (* a 2)))
+
+(f 5)
+
--- ch1/ex1.3.rkt
+++ ch1/ex1.3.rkt
@@ -1,4 +1,6 @@
 #lang sicp
+;; Ex. 1.3: Procedure that takes three numbers as arguments and
+;; returns sum of squares of two larger numbers
 (define (square a)
   (* a a))
 (define (sum-squares a b)
@@ -7,6 +9,6 @@
   (cond ((and (>= a b) (>= b c)) (sum-squares a b))
         ((and (>= a b) (>= c b)) (sum-squares a c))
         ((and (>= b c) (>= c a)) (sum-squares b c))))
-(sq2largest 5 3 3)
+(sq2largest 2 5 3)
 (sq2largest 9 7 8)
 (sq2largest 5 4 6)
