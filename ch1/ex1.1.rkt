49af8268cfd9c0aab9798d3d6eafc21a9cf6f2e3
Fred Luartes <fjluartes@gmail.com>
2019-09-09 19:55:19 +0800

    Edits 1.1 and 1.2

--- ch1/ex1.1.rkt
+++ ch1/ex1.1.rkt
@@ -1,13 +1,26 @@
 #lang sicp
 ;; Ex. 1.1 
-(define (square a)
-  (* a a))
-
-(define (sum-of-squares a b)
-  (+ (square a) (square b)))
-
-(define (f a)
-  (sum-of-squares (+ a 1) (* a 2)))
-
-(f 5)
-
+10
+;; 10
+(+ 5 3 4)
+;; 12
+(- 9 1)
+;; 8
+(/ 6 2)
+;; 3
+(+ (* 2 4) (- 4 6))
+(define a 3)
+(define b (+ a 1))
+(+ a b (* a b))
+(= a b)
+(if (and (> b a) (< b (* a b)))
+    b
+    a)
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+(else 25))
+(+ 2 (if (> b a) b a))
+(* (cond ((> a b) a)
+((< a b) b)
+(else -1))
+(+ a 1))
