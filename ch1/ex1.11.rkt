5a592f5f3db810f114bf73d720aba612c691d396
fjgl <fjluartes@gmail.com>
2019-10-12 15:58:13 +0800

    Adds ex1.11 iterative solution

--- ch1/ex1.11.rkt
+++ ch1/ex1.11.rkt
@@ -1,12 +1,19 @@
 #lang sicp
 ;; Ex 1.11: function f (recursive and iterative process)
 ;; iterative
-
+(define (f n)
+  (f-iter 4 2 1 n))
+(define (f-iter a b c count)
+  (if (= count 1)
+      c
+      (f-iter (+ a
+                 (* b 2)
+                 (* c 3)) a b (- count 1))))
 
 ;; recursive
-(define (f n)
-  (if (< n 3)
-      n
-      (+ (f (- n 1))
-         (* (f (- n 2)) 2)
-         (* (f (- n 3)) 3))))
+;; (define (f n)
+;;   (if (< n 3)
+;;       n
+;;       (+ (f (- n 1))
+;;          (* (f (- n 2)) 2)
+;;          (* (f (- n 3)) 3))))
