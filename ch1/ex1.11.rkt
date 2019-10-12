9dee54d6c2782ed9b2ae3ac4d6ab0adde6d8f0e4
fjgl <fjluartes@gmail.com>
2019-10-12 15:26:03 +0800

    Adds ex1.11 recursive answer

--- ch1/ex1.11.rkt
+++ ch1/ex1.11.rkt
@@ -1,2 +1,12 @@
 #lang sicp
 ;; Ex 1.11: function f (recursive and iterative process)
+;; iterative
+
+
+;; recursive
+(define (f n)
+  (if (< n 3)
+      n
+      (+ (f (- n 1))
+         (* (f (- n 2)) 2)
+         (* (f (- n 3)) 3))))
